e3b63c4be8c8f49b4660a44c6777b5d3a9133568
Ardit Zubaku <zubaku92@gmail.com>
2025-06-22 02:29:43 +0200

    Creating a Web Server with Node Core

--- http-web-server/server.js
+++ http-web-server/server.js
@@ -1,22 +1,56 @@
-'use strict'
-const http = require('http')
-const PORT = process.env.PORT || 3000
+"use strict";
+const { URL } = require("node:url");
+const http = require("node:http");
+const { log } = require("node:util");
+const PORT = process.env.PORT || 3000;
+const { STATUS_CODES } = http;
 
 const hello = `<html>
   <head>
     <style>
      body { background: #333; margin: 1.25rem }
      h1 { color: #EEE; font-family: sans-serif }
-    </style>
+   </style>
   </head>
   <body>
     <h1>Hello World</h1>
   </body>
-</html>`
+</html>`;
+
+const root = `<html>
+<head>
+  <style>
+   body { background: #333; margin: 1.25rem }
+   a { color: yellow; font-size: 2rem; font-family: sans-serif }
+  </style>
+</head>
+<body>
+  <a href='/hello'>Hello</a>
+</body>
+</html>
+`;
 
 const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'text/html')
-  res.end(hello)
-})
+  res.setHeader("Content-Type", "text/html");
+  if (req.method !== "GET") {
+    res.statusCode = 405;
+    res.end(STATUS_CODES[res.statusCode] + "\r\n");
+    return;
+  }
+  // const { pathname } = url.parse(req.url) // Deprecated: Use the WHATWG URL API instead.
+  // https://nodejs.org/dist/latest-v22.x/docs/api/url.html#whatwg-api
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+  if (pathname === "/") {
+    // no need to set res.statusCode because the default res.statusCode is 200 (OK).
+    res.end(root);
+    return;
+  }
+  if (pathname === "/hello") {
+    res.end(hello);
+    return;
+  }
+  res.statusCode = 404;
+  res.end(STATUS_CODES[res.statusCode] + "\r\n");
+});
 
-server.listen(PORT)
+server.listen(PORT);
