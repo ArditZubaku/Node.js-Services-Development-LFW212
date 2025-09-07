c7f91f1f72e3c2261d65533d547b1e8876db4565
Ardit Zubaku <zubaku92@gmail.com>
2025-09-07 04:45:29 +0200

    Serving Static Content with Fastify

--- fastify-web-server/app.js
+++ fastify-web-server/app.js
@@ -3,9 +3,19 @@
 const path = require("node:path");
 const AutoLoad = require("@fastify/autoload");
 
+const dev = process.env.NODE_ENV !== "production";
+
+const fastifyStatic = dev && require("@fastify/static");
+
 const options = {};
 
 module.exports = async function (app, opts) {
+  if (dev) {
+    app.register(fastifyStatic, {
+      root: path.join(__dirname, "public"),
+    });
+  }
+
   app.register(AutoLoad, {
     dir: path.join(__dirname, "plugins"),
     options: Object.assign({}, opts),
--- fastify-web-server/routes/hello/index.js
+++ fastify-web-server/routes/hello/index.js
@@ -1,20 +1,7 @@
 "use strict";
 
-const hello = `<html>
-  <head>
-    <style>
-     body { background: #333; margin: 1.25rem }
-     h1 { color: #EEE; font-family: sans-serif }
-    </style>
-  </head>
-  <body>
-    <h1>Hello World</h1>
-  </body>
-</html>`;
-
-module.exports = async function (app, opts) {
-  app.get("/", async function (req, res) {
-    res.type("text/html");
-    return hello;
+module.exports = async (app, opts) => {
+  app.get("/", async (req, res) => {
+    return res.sendFile("hello.html");
   });
 };
