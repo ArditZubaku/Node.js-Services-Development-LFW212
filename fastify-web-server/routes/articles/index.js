8bab84b7e457b85de6f5b20c228064d300674111
Ardit Zubaku <zubaku92@gmail.com>
2025-09-07 06:19:42 +0200

    Streaming Content with Fastify

---
+++ fastify-web-server/routes/articles/index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const hnLatestStream = require("hn-latest-stream");
+
+module.exports = async (app, opts) => {
+  app.get("/", async (req, res) => {
+    const { amount = 10, type = "html" } = req.query;
+
+    if (type === "html") res.type("text/html");
+    if (type === "json") res.type("application/json");
+
+    return hnLatestStream(amount, type);
+  });
+};
