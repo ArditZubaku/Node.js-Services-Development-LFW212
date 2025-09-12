c174dcdbdec0daedb3d9d8156d15c7227429ba0e
ArditZubaku <zubaku92@gmail.com>
2025-09-12 12:27:57 +0200

    Manipulating Data with RESTful Services: Implementing POST, PUT and DELETE with Express

--- my-express-service/model.js
+++ my-express-service/model.js
@@ -11,8 +11,34 @@ function bicycleModel() {
   };
 
   return {
+    create,
     read,
+    update,
+    del,
+    uid,
   };
+
+  function uid() {
+    return (
+      Object.keys(db)
+        .sort((a, b) => a - b)
+        .map(Number)
+        .filter((n) => !isNaN(n))
+        .pop() +
+      1 +
+      ""
+    );
+  }
+
+  function create(id, data, cb) {
+    if (db.hasOwnProperty(id)) {
+      const err = Error("resource exists");
+      setImmediate(() => cb(err));
+      return;
+    }
+    db[id] = data;
+    setImmediate(() => cb(null, id));
+  }
 
   function read(id, cb) {
     if (!db.hasOwnProperty(id)) {
@@ -23,7 +49,23 @@ function bicycleModel() {
     setImmediate(() => cb(null, db[id]));
   }
 
-  function read(id, cb) {
-    setImmediate(() => cb(Error()));
+  function update(id, data, cb) {
+    if (!db.hasOwnProperty(id)) {
+      const err = Error("not found");
+      setImmediate(() => cb(err));
+      return;
+    }
+    db[id] = data;
+    setImmediate(() => cb());
+  }
+
+  function del(id, cb) {
+    if (!db.hasOwnProperty(id)) {
+      const err = Error("not found");
+      setImmediate(() => cb(err));
+      return;
+    }
+    delete db[id];
+    setImmediate(() => cb());
   }
 }
