3c6058dc383847f7cf40d2ecc3114133988e0059
ArditZubaku <zubaku92@gmail.com>
2025-09-12 15:17:41 +0200

    Consuming and Aggregating Services: Combining Data

--- services/consuming-service/routes/root.js
+++ services/consuming-service/routes/root.js
@@ -1,14 +1,32 @@
 "use strict";
-
-const { BICYCLE_SERVICE_PORT = 4040 } = process.env;
+const { BICYCLE_SERVICE_PORT = 4040, BRAND_SERVICE_PORT = 5050 } = process.env;
 
 const bicycleSrv = `http://localhost:${BICYCLE_SERVICE_PORT}`;
+const brandSrv = `http://localhost:${BRAND_SERVICE_PORT}`;
 
 module.exports = async function (fastify, opts) {
   fastify.get("/:id", async function (request, reply) {
     const { id } = request.params;
-    const res = await fetch(`${bicycleSrv}/${id}`);
-    const bicycle = res.json();
-    return bicycle;
+    const noop = Function.prototype;
+    const signal = AbortSignal.timeout(3000);
+    const bicycleReq = await fetch(`${bicycleSrv}/${id}`, { signal });
+    const brandReq = await fetch(`${brandSrv}/${id}`, { signal });
+    const bicycleJsonPromise = bicycleReq.json();
+    const brandJsonPromise = brandReq.json();
+    bicycleJsonPromise.catch(noop);
+    brandJsonPromise.catch(noop);
+    const results = await Promise.allSettled([
+      bicycleJsonPromise,
+      brandJsonPromise,
+    ]);
+    for (const { reason } of results) if (reason) throw reason;
+    const [bicycle, brand] = results.map(({ value }) => value);
+    return {
+      // If at all possible, avoid sending back the same data that a user sent to the service.
+      // See OWASP, Testing for Reflected Cross Site Scripting for more information on reflected attacks.
+      id: bicycle.id,
+      color: bicycle.color,
+      brand: brand.name,
+    };
   });
 };
