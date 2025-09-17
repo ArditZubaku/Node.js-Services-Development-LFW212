1e7b612489900daee29225d1242e5fd6427d7552
ArditZubaku <zubaku92@gmail.com>
2025-09-17 15:02:52 +0200

    Web Security: Handling User Input - Route Validation with Fastify

--- my-service/routes/bicycle/index.js
+++ my-service/routes/bicycle/index.js
@@ -10,15 +10,63 @@ const del = promisify(bicycle.del);
 module.exports = async (app, opts) => {
   const { notFound } = app.httpErrors;
 
-  app.post("/", async (req, res) => {
-    const { data } = req.body;
-    const id = uid();
-    await create(id, data);
-    res.code(201);
-    return { id };
-  });
+  const idSchema = { type: "integer" };
+  const paramsSchema = {
+    type: "object",
+    properties: {
+      id: idSchema,
+    },
+  };
 
-  app.post("/:id/update", async (req, res) => {
+  const dataSchema = {
+    type: "object",
+    required: ["brand", "color"],
+    additionalProperties: false,
+    properties: {
+      brand: { type: "string" },
+      color: { type: "string" },
+    },
+  };
+  const bodySchema = {
+    // the schema is applied (conceptually) to the body after it has been parsed into a JavaScript object.
+    type: "object",
+    required: ["data"],
+    additionalProperties: false,
+    properties: {
+      data: dataSchema,
+    },
+  };
+
+  const schema = {
+    params: paramsSchema,
+    body: bodySchema,
+  };
+
+  app.post(
+    "/",
+    {
+      schema: {
+        body: bodySchema,
+        response: {
+          201: {
+            type: "object",
+            properties: {
+              id: idSchema,
+            },
+          },
+        },
+      },
+    },
+    async (request, reply) => {
+      const { data } = request.body;
+      const id = uid();
+      await create(id, data);
+      reply.code(201);
+      return { id };
+    }
+  );
+
+  app.post("/:id/update", { schema }, async (req, res) => {
     const { id } = req.params;
     const { data } = req.body;
     try {
@@ -30,7 +78,7 @@ module.exports = async (app, opts) => {
     }
   });
 
-  app.put("/:id", async (req, res) => {
+  app.put("/:id", { schema }, async (req, res) => {
     const { id } = req.params;
     const { data } = req.body;
     try {
@@ -47,24 +95,41 @@ module.exports = async (app, opts) => {
     }
   });
 
-  app.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-      await del(id);
-      res.code(204);
-    } catch (err) {
-      if (err.message === "not found") throw notFound();
-      throw err;
+  app.delete(
+    "/:id",
+    {
+      schema: {
+        params: paramsSchema,
+      },
+    },
+    async (req, res) => {
+      const { id } = req.params;
+      try {
+        await del(id);
+        res.code(204);
+      } catch (err) {
+        if (err.message === "not found") throw notFound();
+        throw err;
+      }
     }
-  });
+  );
 
-  app.get("/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-      return await read(id);
-    } catch (err) {
-      if (err.message === "not found") throw notFound();
-      throw err;
+  app.get(
+    "/:id",
+    {
+      schema: {
+        params: paramsSchema,
+        response: { 200: dataSchema },
+      },
+    },
+    async (req, res) => {
+      const { id } = req.params;
+      try {
+        return await read(id);
+      } catch (err) {
+        if (err.message === "not found") throw notFound();
+        throw err;
+      }
     }
-  });
+  );
 };
