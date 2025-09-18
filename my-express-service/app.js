9827f3fd5a1a1889c8af4e86cab9c404e8377b33
ArditZubaku <zubaku92@gmail.com>
2025-09-18 10:09:21 +0200

    Web Security: Mitigating Attacks - Block an Attackers' IP Address with Express

--- my-express-service/app.js
+++ my-express-service/app.js
@@ -9,6 +9,20 @@ var bicycleRouter = require("./routes/bicycle");
 var app = express();
 
 app.use(logger("dev"));
+// Registering the IP blocking middleware as early as possible makes sense
+// since we don't want an attacker to be able to access any systems.
+// However, it can also be argued that we want to register it after logging middleware.
+// Either way the IP is blocked, but in the latter case the act of it being blocked is visible in the logs.
+app.use(function (req, res, next) {
+  console.log({ addr: req.socket.remoteAddress });
+  if (req.socket.remoteAddress === "127.0.0.1") {
+    const err = new Error("Forbidden");
+    err.status = 403;
+    next(err);
+    return;
+  }
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
