61dcafe1c066744a71e66f9046c7f432db7f0bb9
ArditZubaku <zubaku92@gmail.com>
2025-09-18 10:35:47 +0200

    Lab 13: Block an Attacker's IP Address with Express

---
+++ lab-exercises/lab-13/README.md
@@ -0,0 +1,3 @@
+# Block an Attacker's IP Address with Express
+
+Imagine this is a deployed service, which is receiving a DoS attack from the IP address `111.34.55.211`. Edit the service so that this IP address, and only this IP address, receives a `403 Forbidden` response from the service.
---
+++ lab-exercises/lab-13/app.js
@@ -0,0 +1,44 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use((req, res, next) => {
+  if (req.socket.remoteAddress === "111.34.55.211") {
+    next(createError(403, "Forbidden"));
+    return;
+  }
+  next();
+});
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // send error response as JSON
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
+});
+
+module.exports = app;
